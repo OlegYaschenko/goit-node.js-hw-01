4a37a7c83d469d51b12425ec7df9c6be5f1808a1
OlegYaschenko <olegyaschenko@gmail.com>
2022-06-15 20:18:15 +0300

    Full homework

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # goit-node.js-hw-01
+
+# Получаем и выводим весь список контактов в виде таблицы (console.table)
+
+node index.js --action list
+https://ibb.co/QMdXw17
+
+# Получаем контакт по id
+
+node index.js --action get --id 5
+https://ibb.co/W3Zrzhn
+
+# Добавялем контакт
+
+node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+https://ibb.co/Bz0qjzg
+
+# Удаляем контакт
+
+node index.js --action remove --id=3
+https://ibb.co/KVdNMcr
---
+++ contacts.js
@@ -0,0 +1,56 @@
+const fs = require("fs/promises");
+
+const path = require("path");
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+const { v4: uuidv4 } = require("uuid");
+
+// TODO: задокументировать каждую функцию
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts;
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === contactId);
+  if (!result) {
+    return null;
+  }
+  return result;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex((contact) => contact.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  const [removeContact] = contacts.splice(idx, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return removeContact;
+}
+
+async function addContact(name, email, phone) {
+  if (!name || !email || !phone) {
+    return null;
+  }
+  const newContact = {
+    id: uuidv4(),
+    name,
+    email,
+    phone,
+  };
+  const contacts = await listContacts();
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
